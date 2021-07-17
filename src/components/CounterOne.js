3f937fb05e357dd572946df762d477e1bb2865c5
AndresNeisa <cneisa94@gmail.com>
2021-07-17 14:55:20 -0400

    Update CounterOne.js

--- src/components/CounterOne.js
+++ src/components/CounterOne.js
@@ -2,20 +2,28 @@ import React, { useReducer } from "react";
 
 const initialState = {
   firstCounter: 0,
+  secondCounter: 0,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
       return {
-        firstCounter: state.firstCounter + action.value,
+        ...state,
+        [`${action.counter}Counter`]:
+          state[`${action.counter}Counter`] + action.value,
       };
     case "decrement":
       return {
-        firstCounter: state.firstCounter - action.value,
+        ...state,
+        [`${action.counter}Counter`]:
+          state[`${action.counter}Counter`] - action.value,
       };
     case "reset":
-      return initialState;
+      return {
+        ...state,
+        [`${action.counter}Counter`]: initialState[`${action.counter}Counter`],
+      };
     default:
       return state;
   }
@@ -24,22 +32,77 @@ const reducer = (state, action) => {
 const CounterOne = () => {
   const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div>
-      <div>Count - {count.firstCounter}</div>
-      <button onClick={() => dispatch({ type: "increment", value: 1 })}>
-        Increment
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
-        Decrement
-      </button>
-      <button onClick={() => dispatch({ type: "increment", value: 5 })}>
-        Increment 5
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", value: 5 })}>
-        Decrement 5
-      </button>
-      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
-    </div>
+    <React.Fragment>
+      <div>
+        <div>Count - {count.firstCounter}</div>
+        <button
+          onClick={() =>
+            dispatch({ type: "increment", value: 1, counter: "first" })
+          }
+        >
+          Increment
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: "decrement", value: 1, counter: "first" })
+          }
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: "increment", value: 5, counter: "first" })
+          }
+        >
+          Increment 5
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: "decrement", value: 5, counter: "first" })
+          }
+        >
+          Decrement 5
+        </button>
+        <button onClick={() => dispatch({ type: "reset", counter: "first" })}>
+          Reset
+        </button>
+      </div>
+
+      <div>
+        <div>Count - {count.secondCounter}</div>
+        <button
+          onClick={() =>
+            dispatch({ type: "increment", value: 1, counter: "second" })
+          }
+        >
+          Increment
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: "decrement", value: 1, counter: "second" })
+          }
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: "increment", value: 5, counter: "second" })
+          }
+        >
+          Increment 5
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: "decrement", value: 5, counter: "second" })
+          }
+        >
+          Decrement 5
+        </button>
+        <button onClick={() => dispatch({ type: "reset", counter: "second" })}>
+          Reset
+        </button>
+      </div>
+    </React.Fragment>
   );
 };
 
